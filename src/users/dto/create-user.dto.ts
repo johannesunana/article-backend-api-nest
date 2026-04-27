0e0a2dcbdafd8330a010db91237f8cda68ccb104
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-04-27 13:56:52 +0800

    feat(users): add validation for create user dto
    - add `class-validator` as project dependency
    - define validation rules for `create-user.dto` fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsInt, IsDate } from 'class-validator'
+
+export class CreateUsersDto {
+  @IsString()
+  name: string;
+  username: string;
+  password: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsInt()
+  id: Uint16Array;
+
+  @IsDate
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Reference: https://dev.to/abhivyaktii/understanding-dtos-data-transfer-objects-in-nestjs-52pb
