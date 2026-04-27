1e224e7f1b2432b970962a39869daee34b418709
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-04-27 17:16:54 +0800

    feat(auth): add jwt login token generation
    - move login auth in`AuthService` with `PrismaService`
    - validate user credentials with `bcrypt` before issuing token
    - generate and return jwt on successful login
    - export `AuthService` and add auth response dto for token output

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [JwtModule.register({ secret: process.env.JWT_SECRET })],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PrismaService],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,51 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { LoginEmailDto } from '../users/dto/login-email.dto';
+import { LoginUsernameDto } from '../users/dto/login-username.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    // private readonly usersService: UsersService,
-    // private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
   ) {}
-  
+
+  async login(loginDto: LoginEmailDto | LoginUsernameDto) {
+    let user: string | any;
+
+    if ('email' in loginDto) {
+      user = await this.prisma.user.findUnique({
+        where: {
+          email: loginDto.email,
+        },
+      });
+    } else {
+      user = await this.prisma.user.findUnique({
+        where: {
+          username: loginDto.username,
+        },
+      });
+    }
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      username: user.username,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
