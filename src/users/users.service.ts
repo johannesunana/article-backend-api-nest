d27e7736a7209db19cca3648da0a4685abf962ef
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-04-27 11:47:08 +0800

    feat(users): connect users service to Prisma
    - inject `PrismaService` and remove placeholder text in `UsersService`
    - update create and findAll function with prisma client API
    - update prisma client generation to use commonjs format

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
 }
