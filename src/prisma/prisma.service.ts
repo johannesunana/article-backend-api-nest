20ca79305331655950d47ca191769a705864dfdf
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-04-29 11:24:01 +0800

    chore(prisma): remove database url console log
    - remove `console.log()`from `prisma.service.ts`

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,6 @@ export class PrismaService extends PrismaClient {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL as string,
     });
-    console.log(process.env.DATABASE_URL);
     super({ adapter });
   }
 }
