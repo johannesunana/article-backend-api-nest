4d1c9d32ffa6ef0caf8bd74d1da01ffc1e7a80d8
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-05-04 13:37:46 +0800

    feat(auth): add passport strategies
    - add `PassportModule` to `src/auth/auth.module.ts`
    - add `LocalStrategy` and `JwtStrategy`
    - add JWT auth constants in `src/auth/constants.ts`

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
+    PassportModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
       signOptions: {expiresIn: '1h'}}),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
