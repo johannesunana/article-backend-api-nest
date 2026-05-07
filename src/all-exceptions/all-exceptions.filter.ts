4844ee28427e553dd4e93de771a6a8085167248b
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-05-07 15:55:36 +0800

    feat(core): add global exception filter
    - add `AllExceptionsFilter` in `src/all-exceptions/all-exceptions.filter.ts`
    - register the filter globally in `src/main.ts`
    - add a basic spec for the new exception filter

---
+++ src/all-exceptions/all-exceptions.filter.ts
@@ -0,0 +1,23 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+  
+  catch(exception: unknown, host: ArgumentsHost) {
+    this.logger.error(exception);
+
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus();
+      return response.status(status).json(exception.getResponse());
+    }
+
+    return response.status(500).json({
+      statusCode: 500,
+      message: 'Internal server error',
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import morgan from 'morgan';
+import { AllExceptionsFilter } from './all-exceptions/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.use(morgan(':date[iso] - :method :url :status \(:response-time ms\)'));
+  
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+
+  app.useGlobalFilters(new AllExceptionsFilter);
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
