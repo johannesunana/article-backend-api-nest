6b6cb28b151cfb2f445c6e4e9f51b2e52ef9fc61
Johannes Andrei Unana <99703415+johannesunana@users.noreply.github.com>
2026-05-15 17:25:44 +0800

    feat(swagger): add Swagger UI setup in bootstrap
    - import `SwaggerModule` and `DocumentBuilder` in `src/main.ts`
    - configure OpenAPI document metadata for the article backend API
    - expose Swagger UI at `/api`

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './all-exceptions/all-exceptions.filter';
@@ -13,6 +14,15 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new AllExceptionsFilter());
+  
+  const config = new DocumentBuilder()
+    .setTitle('Artlcle Backend API')
+    .setDescription('Simple Backend API for Article Publishing')
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
